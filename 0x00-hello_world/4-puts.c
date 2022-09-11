ca1da508dcc3402d2e20420464cd33c64ca06ba2
Jerryugorji <meetpastorjerry@gmail.com>
2022-09-11 12:40:57 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,10 @@
 /**
 
  * main - print the string in the put function
- * 
+ *
  *Description: using the main function 
  *this program prints "Programming is like building a multilingual puzzle
-
  * Return: 0
-
  **/
 int main(void)
 

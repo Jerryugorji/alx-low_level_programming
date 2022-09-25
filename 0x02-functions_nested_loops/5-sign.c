7c8f6a9383e4990b0ceb16fd26d2b2870483e6ef
Jerryugorji <meetpastorjerry@gmail.com>
2022-09-25 09:29:24 -0700

    Write a function that prints the sign of a number.
    Prototype: int print_sign(int n);
    
    Returns 1 and prints + if n is greater than zero
    
    Returns 0 and prints 0 if n is zero
    
    Returns -1 and prints - if n is less than zero

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /**
-
  * print_sign - function to check for a sign of a number
-
  * @n:  is the int that will use for the argument of the function
-
  * Return: 0
-
  */
 
 int print_sign(int n)
